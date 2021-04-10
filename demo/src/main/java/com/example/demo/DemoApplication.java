dcc2c3a7287712e3861e24976df93b4cb11214f7
Doderovic Milan <mdoderovic5@gmail.com>
2021-04-10 19:44:23 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+  	  @RequestMapping("/test")
+  public String homeTest() {
+    return "Hello Docker World TEST";
+  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
